b4ebe585b6aa5113a0800f40e9cccae7f4a92f46
Carlos Enrique Oropeza <ceon20020508@gmail.com>
2022-11-22 18:47:54 -0500

    Corrigiendo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,6 +22,7 @@
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.0.1/chart.min.js"></script>
 
     <!-- Styles -->
     @livewireStyles
